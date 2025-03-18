4b6a3fe50090855ce7e94b27995cf56cddc82a55
Sneed Group Holder <no@nope.nicetry>
2025-03-18 01:13:30 -0500

    DOM immediately loads and unloads... TF?

--- debug-run.sh
+++ debug-run.sh
@@ -2,5 +2,9 @@ rm debug.log
 rm errors.log
 python3 main.py --debug > debug.log 2>&1 &
 wait
-cat debug.log | grep WARNING > errors.log
+echo "**Broswer warnings BELOW**: " > errors.log
+cat debug.log | grep WARNING >> errors.log
+echo "**Render-level errors BELOW** " >> errors.log
 cat debug.log | grep ERROR >> errors.log
+echo "**Python-level errors BELOW - LIKELY AT LEAST SOMEWHAT SEVERE**" >> errors.log
+cat debug.log | grep Error >> errors.log
